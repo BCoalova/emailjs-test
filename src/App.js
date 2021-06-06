0b23c1eacaaaa8bad5c1cc18e90ea5f7ee3cf4b0
Boris Coalova <68432860+BCoalova@users.noreply.github.com>
2021-06-06 15:25:53 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import emailjs from 'emailjs-com'
+import { Form, Button, Container } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const serviceId = process.env.REACT_APP_API_YOUR_SERVICE_ID
+    const templateId = process.env.REACT_APP_API_YOUR_TEMPLATE_ID
+    const userId = process.env.REACT_APP_API_YOUR_USER_ID
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        emailjs.sendForm(serviceId, templateId, e.target, userId).then(
+            (result) => {
+                console.log(result.text)
+            },
+            (error) => {
+                console.log(error.text)
+            }
+        )
+        e.target.reset()
+    }
+
+    return (
+        <Container>
+            <Form onSubmit={(e) => handleSubmit(e)}>
+                <Form.Group>
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="text" placeholder="name" name="name" />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>email</Form.Label>
+                    <Form.Control type="email" placeholder="Email" name="email" />
+                </Form.Group>
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Example textarea</Form.Label>
+                    <Form.Control as="textarea" rows={3} name="message" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+    )
 }
 
-export default App;
+export default App
